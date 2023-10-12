71d26eae16cda09995bb1adc24615d6600df3fd4
SHEOMM <esh0821@snu.ac.kr>
2023-10-12 16:30:21 +0900

    feat: add drawing-post

--- backend/apps/drawing/urls.py
+++ backend/apps/drawing/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DrawingSubmitAPIView
+from .views import DrawingSubmitAPIView, DrawingAPIView
 
 urlpatterns = [
+    path('', DrawingAPIView.as_view(), name='drawing-list'),
     path('<int:id>/submit', DrawingSubmitAPIView.as_view(), name='drawing-submit'),
 ]
--- backend/apps/drawing/views.py
+++ backend/apps/drawing/views.py
@@ -3,7 +3,7 @@ from rest_framework import status, views
 from rest_framework.response import Response
 from django.conf import settings
 from .models import Drawing
-from .serializers import DrawingSerializer
+from .serializers import DrawingSerializer, DrawingCreateSerializer
 
 
 # Create your views here.
@@ -17,6 +17,14 @@ class DrawingAPIView(views.APIView):
         drawings = Drawing.objects.filter(host_id=user_id).prefetch_related('participants')
         serializer = DrawingSerializer(drawings, many=True)
         return Response({"drawings": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = DrawingCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DrawingSubmitAPIView(views.APIView):
 
