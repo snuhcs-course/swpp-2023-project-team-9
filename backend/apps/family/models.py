7b7fdda6163422f9ee8fa6c1216d3700bf5806dd
Jonghyun Choe <andy.choe729@gmail.com>
2023-10-12 13:53:12 +0900

    feat: add Family model

--- backend/apps/family/models.py
+++ backend/apps/family/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Family(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    host_id = models.ForeignKey('user.User', on_delete=models.CASCADE, db_column="host_id", related_name='host_family')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
