24d843c25ad390589a5e045b3c79f4882e3577db
jonghyunchoe <andy.choe729@gmail.com>
2023-10-17 13:43:02 +0900

    feat: add sign up serializer

---
+++ backend/apps/user/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'username': instance.username,
+            'gender': instance.gender,
+            'type': instance.type,
+            'created_at': instance.created_at
+        }
