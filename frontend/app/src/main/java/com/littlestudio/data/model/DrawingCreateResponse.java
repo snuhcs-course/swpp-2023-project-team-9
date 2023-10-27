6e974060fa2904359fa51017dfabe1eabfeefa50
jonghyunchoe <andy.choe729@gmail.com>
2023-10-28 02:14:42 +0900

    feat (model): add api request and response models

---
+++ frontend/app/src/main/java/com/littlestudio/data/model/DrawingCreateResponse.java
@@ -0,0 +1,10 @@
+package com.littlestudio.data.model;
+
+import java.util.Date;
+
+public class DrawingCreateResponse {
+    public int id;
+    public int host_id;
+    public String invitation_code;
+    public Date created_at;
+}
