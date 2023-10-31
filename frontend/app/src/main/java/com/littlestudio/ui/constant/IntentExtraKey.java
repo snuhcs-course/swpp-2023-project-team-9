3dc7baeab4eb7921fe6e002072e563186149904c
gunhee-c <chorjgml@naver.com>
2023-11-01 02:40:15 +0900

    feat: set enum class for drawing Code

--- frontend/app/src/main/java/com/littlestudio/ui/constant/IntentExtraKey.java
+++ frontend/app/src/main/java/com/littlestudio/ui/constant/IntentExtraKey.java
@@ -2,4 +2,5 @@ package com.littlestudio.ui.constant;
 
 public class IntentExtraKey {
     public final static String INVITATION_CODE = "invitationCode";
+    public final static String DRAWING_CODE = "drawingCode";
 }
