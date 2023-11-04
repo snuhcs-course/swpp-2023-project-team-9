8ba93be8f7abbf23a9dbb7193eca00355f44203f
Jonghyun Choe <andy.choe729@gmail.com>
2023-11-04 12:25:38 +0900

    feat: add UserFamily model to change family policy

--- backend/apps/family/models.py
+++ backend/apps/family/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 class Family(models.Model):
     id = models.BigAutoField(primary_key=True)
-    host_id = models.ForeignKey('user.User', on_delete=models.CASCADE, db_column="host_id", related_name='host_family')
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, db_column="user_id", related_name='user_family')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class UserFamily(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    family_id = models.ForeignKey(Family, on_delete=models.CASCADE, db_column="family_id", related_name='family_users')
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, db_column="user_id", related_name='user_family_users')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
