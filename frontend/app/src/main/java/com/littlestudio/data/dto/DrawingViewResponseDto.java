3c9afb5bf13b37e2d3a8ada818e0c590d62b6145
Jonghyun Choe <andy.choe729@gmail.com>
2023-11-25 01:09:19 +0900

    fix: change field name

--- frontend/app/src/main/java/com/littlestudio/data/dto/DrawingViewResponseDto.java
+++ frontend/app/src/main/java/com/littlestudio/data/dto/DrawingViewResponseDto.java
@@ -8,9 +8,9 @@ public class DrawingViewResponseDto {
     public String title;
     public String description;
     public String image_url;
-    public String jumping_url;
-    public String dab_url;
-    public String wave_hello_url;
+    public String gif_dab_url;
+    public String gif_jumping_url;
+    public String gif_wave_hello_url;
     public String type;
     public int host_id;
     public List<FamilyMemberResponseDto> participants;
