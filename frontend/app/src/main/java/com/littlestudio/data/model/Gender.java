bc939d79b4659e56df9ee1cf1793520f4260a6bd
dnwns3437 <dnwns3437@gmail.com>
2023-11-26 16:23:10 +0900

    feat (model): update User model

---
+++ frontend/app/src/main/java/com/littlestudio/data/model/Gender.java
@@ -0,0 +1,7 @@
+package com.littlestudio.data.model;
+
+public class Gender {
+    public final static String MALE = "MALE";
+    public final static String FEMALE = "FEMALE";
+    public final static String OTHER = "OTHER";
+}
---
+++ frontend/app/src/main/java/com/littlestudio/data/model/UserType.java
@@ -0,0 +1,6 @@
+package com.littlestudio.data.model;
+
+public class UserType {
+    public final static String PARENT = "PARENT";
+    public final static String CHILD = "CHILD";
+}
