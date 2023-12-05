ebf98da4d5521c3371947060a6ca7e8b5639a915
Jonghyun Choe <andy.choe729@gmail.com>
2023-12-05 19:29:08 +0900

    update: remove django secret key

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tk$lkk_xnyn#9xn*yjn@v+n^27n3wtmmz-oo-0#v84mxoa7=*4"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
