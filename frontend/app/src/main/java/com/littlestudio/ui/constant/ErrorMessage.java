ab922e0bdb247bf677d8405bc02bd3cea52f98f3
Jonghyun Choe <andy.choe729@gmail.com>
2023-12-07 15:16:53 +0900

    update: error message

--- frontend/app/src/main/java/com/littlestudio/ui/constant/ErrorMessage.java
+++ frontend/app/src/main/java/com/littlestudio/ui/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package com.littlestudio.ui.constant;
 
 public class ErrorMessage {
-    public final static String DEFAULT = "An error has occurred. Please try again later.";
+    public final static String DEFAULT = "An error has occurred. Please try again later or check your network connection.";
     public final static String LOGIN = "Please check your ID and password.";
     public final static String EMPTY_INVITATION_CODE = "Please enter the invitation code.";
     public final static String WRONG_INVITATION_CODE = "Invitation code is incorrect.";
